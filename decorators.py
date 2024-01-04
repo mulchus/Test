35f6c14e41fd619de066632abd3ddbff6e370e17
mulchus <mulch@list.ru>
2024-01-04 14:31:17 +0300

    Updated

--- decorators.py
+++ decorators.py
@@ -1,21 +1,42 @@
+# https://tproger.ru/translations/demystifying-decorators-in-python
+
+# def benchmark(func):
+#     import time
+#
+#     def wrapper():
+#         start = time.time()
+#         func()
+#         end = time.time()
+#         print('[*] Время выполнения: {} секунд.'.format(end-start))
+#     return wrapper
+#
+# @benchmark
+# def fetch_webpage():
+#     import requests
+#     print(requests.get('https://google.com').text[:200])
+#
+# # fetch_webpage = benchmark(fetch_webpage)
+# fetch_webpage()
+
 def benchmark(func):
     import time
-
-    def wrapper():
+    
+    def wrapper(*args, **kwargs):
         start = time.time()
-        func()
+        return_value = func(*args, **kwargs)
         end = time.time()
-        print('[*] Время выполнения: {} секунд.'.format(end-start))
+        print('[*] Время выполнения: {} секунд.'.format(end - start))
+        return return_value
+    
     return wrapper
 
-
-# @benchmark
-
-def fetch_webpage():
+@benchmark
+def fetch_webpage(url):
     import requests
-    webpage = requests.get('https://google.com')
+    webpage = requests.get(url)
+    return webpage.text[:200]
 
+# fetch_webpage = benchmark(fetch_webpage)
 
-fetch_webpage = benchmark(fetch_webpage)
-
-fetch_webpage()
+webpage = fetch_webpage('https://google.com')
+print(webpage)
