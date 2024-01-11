ba46a09dc6d36e61832e2fe1b840e35084ac42e8
mulchus <mulch@list.ru>
2024-01-11 16:59:20 +0300

    Added algorithm merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2	# Haxopm cepepuny Maccuea
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        merge(arr, left_half, right_half)
+
+
+def merge(arr, left, right):
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right(j)
+            j += 1
+            k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
+array = [38, 27, 43, 3, 9, 82, 10]
+
+merge_sort(array)
+
+print( "OTCopTMpoBaHHbiM MaccuB:", array)
