64ce4e2cba8e50db5c9f3727751895a0311309e8
mulchus <mulch@list.ru>
2024-04-27 13:03:07 +0300

    add fast sort algorithm

--- merge_sort.py
+++ merge_sort.py
@@ -38,3 +38,8 @@ array = [38, 27, 43, 3, 9, 82, 10]
 merge_sort(array)
 
 print( "OTCopTMpoBaHHbiM MaccuB:", array)
+
+# Алгоритм быстрой сортировки
+unsorted = [33, 2, 3, 45, 6, 54, 33]
+quick = lambda l: quick([x for x in l[1:] if x <= l[0]]) + [l[0]] + quick([x for x in l if x > l[0]]) if l else []
+print(quick(unsorted))
