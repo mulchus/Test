424ca4cd4153374392a173d2f95702fe390171b7
mulchus <mulch@list.ru>
2024-05-03 16:22:04 +0300

    add var 2

--- some_exceptions.py
+++ some_exceptions.py
@@ -3,8 +3,18 @@ try:
 except Exception as e:
     print(e)
     print(f'I catch it into Exception = {e!r}')
-    raise   # and raise another
+    # raise FileNotFoundError  # and raise another
 except TypeError as e:
     print(e)
     print(f'I catch it into TypeError = {e!r}')
+    raise
+
+
+try:
+    raise TypeError('some error')
+except TypeError as e:
+    print(f'I catch it into Exception = {e!r}')
+    raise e  # and raise another
+finally:
+    print('finally')
     
