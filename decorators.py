510716f32ebabca29d97688c704517489df068a3
mulchus <mulch@list.ru>
2024-05-03 13:29:51 +0300

    add lite time decorator

--- decorators.py
+++ decorators.py
@@ -50,3 +50,21 @@ webpage = fetch_webpage('https://google.com')
 
 print(fetch_webpage.__name__)
 print(fetch_webpage.__doc__)
+
+
+import time
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {end - start} seconds to execute.")
+        return result
+    return wrapper
+
+@time_it
+def some_func():
+    time.sleep(1)
+
+some_func()
