a19bc5145a5f8c6cc7a0af15f3d07250dba91902
mulchus <mulch@list.ru>
2024-05-07 11:10:41 +0300

    upd to MIMEText with subject

--- send_mail.py
+++ send_mail.py
@@ -1,15 +1,30 @@
 import smtplib
 from environs import Env
+from email.mime.text import MIMEText
 
 env = Env()
 env.read_env()
 
 sender_email = env("SENDER_EMAIL")
 receiver_email = env("RECEIVER_EMAIL")
-message = "Привет от Питона!"
+subject = "Пробное письмо"
+message = """Привет от Питона!
+            Python comes with a number of codecs built-in, either
+            implemented as C functions or with dictionaries as mapping tables.
+            The following table lists the codecs by name,
+            Если сервер Тильды не получит ожидаемый ответ,
+            то будет еще две попытки повторного запроса на ваш сервер (всего 3 запроса).
+            Запрос может отправляться не сразу, а с задержкой от 1 до 20 минут после публикации.
+            Поэтому, если запрос не пришел сразу, то нужно убедиться, что он не пришел в указанное время.
+          """   # .encode("koi8_r")
+
+mess = MIMEText(message, 'html')
+mess['From'] = sender_email
+mess['To'] = receiver_email
+mess['Subject'] = subject
 
 smtp_server = smtplib.SMTP(env("SMTP_SERVER"), 587)
 smtp_server.starttls()
 smtp_server.login(sender_email, env("SENDER_PASSWORD"))
-smtp_server.sendmail(sender_email, receiver_email, message)
+smtp_server.sendmail(sender_email, receiver_email,  mess.as_string())
 smtp_server.quit()
