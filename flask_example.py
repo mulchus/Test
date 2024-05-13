865b17f37d3d2c0cb6d0706086a3335fecbc6f5b
mulchus <mulch@list.ru>
2024-05-14 00:14:23 +0300

    add template with input and redirect

--- flask_example.py
+++ flask_example.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, render_template, redirect
+import jinja2
+
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'
+loader = jinja2.FileSystemLoader('templates')
 
 
 @app.route('/')
@@ -28,7 +31,39 @@ def get_session():
         return 'Hello, {}'.format(username)
     else:
         return 'No session value set'
-    
+
+
+# модель
+def get_data_from_database():
+    data = {
+        'name': 'Вася',
+        'verb': 'like',
+    }
+    return data
+
+
+# представление
+@app.route('/start')
+def start():
+    name = request.args.get('name')
+    verb = request.args.get('verb')
+    data = get_data_from_database()
+    if name:
+        data['name'] = name
+    if verb:
+        data['verb'] = verb
+    return render_template('index.html', data=data)
+
+
+# контроллер
+@app.route('/submit', methods=['POST'])
+def submit():
+    # код для обработки данных, полученных из формы
+    name = request.form['name']
+    verb = request.form['verb']
+    # сохранение данных в базу данных
+    return redirect(f'/start?name={name}&verb={verb}')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
