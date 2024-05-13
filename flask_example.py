1900285b4017f69bcd9ee132d5efcfadbf78078a
mulchus <mulch@list.ru>
2024-05-13 23:34:49 +0300

    add Flask sessions

--- flask_example.py
+++ flask_example.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, session
 
 app = Flask(__name__)
+app.secret_key = 'some_secret_key'
 
 
 @app.route('/')
@@ -12,6 +13,22 @@ def index():
 def hello():
     name = request.args.get('name')
     return f'Hello, {name, request.__dict__}!'
+
+
+@app.route('/set_session')
+def set_session():
+    session['username'] = 'John'
+    return 'Session value set'
+
+
+@app.route('/get_session')
+def get_session():
+    username = session.get('username')
+    if username:
+        return 'Hello, {}'.format(username)
+    else:
+        return 'No session value set'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
