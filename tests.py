91c7e135230d40e8134f49b9c0d81477ac83ee06
mulchus <mulch@list.ru>
2024-05-13 23:03:47 +0300

    add unittest and pytest examples

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def add_numbers(x, y):
+    return x + y
+
+
+class TestAddNumbers(unittest.TestCase):
+    
+    def test_add_positive_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+    
+    def test_add_negative_numbers(self):
+        self.assertEqual(add_numbers(-2, -3), -5)
+    
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add_numbers(-2, 3), 1)
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
