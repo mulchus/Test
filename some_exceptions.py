07e83053eb655a7bc3cd9991b50a0060a937f51d
mulchus <mulch@list.ru>
2024-05-15 14:12:21 +0300

    add full variant of exception block

--- some_exceptions.py
+++ some_exceptions.py
@@ -1,4 +1,17 @@
 try:
+    x = int(input("Введите число: "))
+    y = 1 / x
+except ZeroDivisionError:
+    print("На ноль делить нельзя!")
+except ValueError:
+    print("Вы ввели не число!")
+else:
+    print("Результат: ", y)
+finally:
+    print("Конец программы")
+
+
+try:
     raise TypeError('some error')
 except Exception as e:
     print(e)
