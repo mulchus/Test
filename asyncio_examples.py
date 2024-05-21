7a48ce6d5d09c55bc4a0f382ec1db843e2401b8e
mulchus <mulch@list.ru>
2024-05-21 12:46:07 +0300

    add example with cancel task that didn't finish

--- asyncio_examples.py
+++ asyncio_examples.py
@@ -12,4 +12,19 @@ async def make_request(url):
 loop = asyncio.get_event_loop()
 some_url = "https://www.example.com"
 result_html = loop.run_until_complete(make_request(some_url))
-print(result_html)
+# print(result_html)
+
+
+# example with cancel task that doesn't finished
+async def test():
+    print(1)
+    await asyncio.sleep(10)
+    print(2)
+
+
+async def main():
+    task = asyncio.create_task(test())
+    await asyncio.sleep(2)
+    task.cancel()
+
+asyncio.run(main())
