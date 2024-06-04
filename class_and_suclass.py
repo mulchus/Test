563e96e8b34f376bbb74ae29cb35c2d76d1102a0
mulchus <mulch@list.ru>
2024-06-04 12:09:16 +0300

    add example of inheritance

--- class_and_suclass.py
+++ class_and_suclass.py
@@ -137,3 +137,42 @@ student = Student("John", 20, "Computer Science")
 teacher = Teacher("Jane", 30, "Computer Science")
 student.introduce()
 teacher.introduce()
+
+
+# пример того, как ООП можно использовать при передаче параметров в конструктор
+class Car:
+    def __init__(self, type):
+        self.type = type
+        self.car_properties = {}
+
+    def set_properties(self, color, gear, capacity):
+        self.car_properties = {"Color": color, "Gear": gear, "Capacity": capacity}
+
+    def get_properties(self):
+        return self.car_properties
+
+
+class PetrolCar(Car):
+    def __init__(self, type):
+        self.type = type
+        self.car_properties = {}
+
+
+car = Car("SUV")
+car.set_properties("Red", "Auto", 6)
+
+petrol_car = PetrolCar("Sedan")
+petrol_car.set_properties("Blue", "Manual", 4)
+
+cars = [car, petrol_car]
+
+
+def find_red_cars(cars):
+    red_cars = 0
+    for car in cars:
+        if car.get_properties()['Color'] == "Red":
+            red_cars += 1
+    print(f'Number of Red Cars = {red_cars}')
+
+
+find_red_cars(cars)
