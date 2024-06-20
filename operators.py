113fd6996e20b5b93fdde694080f6c8b0ef79b7c
mulchus <mulch@list.ru>
2024-06-20 18:12:51 +0300

    added examples with boolean operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+
+print(True and 'Карась!')
+print('Щука!' and 'Карась!')
+print(4 < 5 and 'Карась!')
+print()
+
+print(False and 'Карась!')
+print(0 and 'Карась!')
+print(4 > 5 and 'Карась!')
+print()
+
+print(True or 'Карась!')
+print('Щука!' or 'Карась!')
+print(4 < 5 or 'Карась!')
+print()
+
+print(False or 'Карась!')
+print(0 or 'Карась!')
+print(4 > 5 or 'Карась!')
+print()
