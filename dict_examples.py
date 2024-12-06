6296f42e5238d314a643a56b63c740eb51e72323
mulchus <mulch@list.ru>
2024-12-06 19:52:39 +0300

    Add compare two dicts.

--- dict_examples.py
+++ dict_examples.py
@@ -30,3 +30,21 @@ dict2 = {'c': 3, 'd': 4}
 chain_map = ChainMap(dict1, dict2)
 print(chain_map)
 print(dict(chain_map))
+
+
+def compare_templates_indexes(curent_templates_index: dict, new_templates_index: dict) -> tuple:
+    curent_templates_index_keys = set(curent_templates_index.keys())
+    new_templates_index_keys = set(new_templates_index.keys())
+    shared_keys = curent_templates_index_keys.intersection(new_templates_index_keys)
+    added = curent_templates_index_keys - new_templates_index_keys
+    removed = new_templates_index_keys - curent_templates_index_keys
+    modified = ({o: (curent_templates_index[o], new_templates_index[o])
+                for o in shared_keys if curent_templates_index[o] != new_templates_index[o]})
+    same = set(o for o in shared_keys if curent_templates_index[o] == new_templates_index[o])
+    return added, removed, modified, same
+
+
+x = {"a": 1, "d": 2, 'z': 3, 'p': 20}
+y = {"a": 12, "c": 4, 'b': 5, 'p': 20}
+added, removed, modified, same = compare_templates_indexes(y, x)
+print(added, removed, modified, same)
