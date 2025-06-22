7209e8186df76a9fa5e62df62b42fc19207574cb
mulchus <mulch@list.ru>
2025-06-22 16:46:52 +0300

    Update context_manager examples

--- context_manager.py
+++ context_manager.py
@@ -15,34 +15,75 @@ class Timer:
         elapsed_time = stoped_time - self.start_time
         print(f"Timer stopped at {stoped_time}")
         print(f"Elapsed time: {elapsed_time} seconds")
+        print()
 
 
 with Timer() as timer:
     # Ваш блок кода
-    # time.sleep(1)
-    ...
+    time.sleep(1)
+
+
+# Еще пример использования синхронного контекстного менеджера с использованием декоратора
+from contextlib import contextmanager
+
+@contextmanager
+def timer():
+    start_time = time.time()
+    print(f"Timer started at {start_time}")
+    yield start_time
+    stoped_time = time.time()
+    print(f"Timer stopped at {stoped_time}")
+    elapsed_time = stoped_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+    print()
+
+
+with timer() as timer:
+    print("Ваш синхронный блок кода")
+    time.sleep(1.2)
 
 
 # Пример использования асинхронного контекстного менеджера
 class AsyncTimer:
-    def __enter__(self):
+    async def __aenter__(self):
         self.start_time = asyncio.get_event_loop().time()
         print(f"Timer started at {self.start_time}")
         return self
 
-    def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
         stoped_time = asyncio.get_event_loop().time()
         print(f"Timer stopped at {stoped_time}")
         elapsed_time = stoped_time - self.start_time
         print(f"Elapsed time: {elapsed_time} seconds")
+        print()
 
 
 async def example():
-    async with AsyncTimer() as timer:
+    async with AsyncTimer() as timering:
         print("Ваш асинхронный блок кода")
-        # Ваш асинхронный блок кода
-        # timer.sleep(1)
         await asyncio.sleep(2)
 
 # Запуск асинхронной функции
+asyncio.run(example())
+
+
+# Еще пример использования асинхронного контекстного менеджера
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def async_timer():
+    start_time = asyncio.get_event_loop().time()
+    print(f"Timer started at {start_time}")
+    yield start_time
+    stoped_time = asyncio.get_event_loop().time()
+    print(f"Timer stopped at {stoped_time}")
+    elapsed_time = stoped_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+    print()
+
+async def example():
+    async with async_timer() as timer:
+        print("Ваш асинхронный блок кода")
+        await asyncio.sleep(1.5)
+
 asyncio.run(example())
