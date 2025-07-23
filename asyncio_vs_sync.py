2262a1506b077aa1d8a4cd23c07accb7516ec03a
mulchus <mulch@list.ru>
2025-07-23 23:24:24 +0300

    Update asyncio vs sync examples

--- asyncio_vs_sync.py
+++ asyncio_vs_sync.py
@@ -37,3 +37,49 @@ asyncio.run(main2(["https://example.com"] * REQUEST_COUNT))
 
 # main took 7.979214906692505 seconds to execute.
 # main2 took 1.1470463275909424 seconds to execute.
+
+
+# Example 2
+
+from asyncio import gather, run
+from datetime import datetime
+
+
+async def main():
+    start_time = datetime.now()
+    await gather(task1(), task2(), )
+    print("Delay: ", datetime.now() - start_time)
+
+
+while True:
+    var = input('Вар 1 или 2? ')
+
+    match var:
+        case '1':
+            from time import sleep
+
+            async def task2():
+                for i in range(5):
+                    sleep(0.5)
+                    print(f"Task 2: {i}")
+
+            async def task1():
+                for i in range(5):
+                    sleep(1)
+                    print(f"Task 1: {i}")
+
+        case '2':
+            from asyncio import sleep
+
+            async def task2():
+                for i in range(5):
+                    await sleep(0.5)
+                    print(f"Task 2: {i}")
+
+            async def task1():
+                for i in range(5):
+                    await sleep(1)
+                    print(f"Task 1: {i}")
+
+    run(main())
+
