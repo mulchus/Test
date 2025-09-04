4800acfa9bbda00ed3ce70ed5e8ab13e5d890ad1
mulchus <mulch@list.ru>
2025-09-04 21:00:25 +0300

    Update by append dynamic methods

--- class_methods.py
+++ class_methods.py
@@ -2,19 +2,43 @@ class MyClass:
     def __init__(self, name):
         self.name = name
 
-    def method3(self):
+    def instance_method(self):
         return f"вызван метод инстанса {self.name} класса"
 
     @staticmethod
-    def method2():
+    def static_method():
         return f"вызван статический метод класса"
 
     @classmethod
-    def method1(cls):
+    def class_method(cls):
         return f"вызван метод класса {cls}"
 
 
-print(MyClass.method1(), str(MyClass.method1))
-print(MyClass.method2(), str(MyClass.method2))
-print(MyClass('A').method3(), str(MyClass('A').method3))
-print(MyClass('B').method3(), str(MyClass('B').method3))
+print(MyClass.class_method(), str(MyClass.class_method))
+print(MyClass.static_method(), str(MyClass.static_method))
+print(MyClass('A').instance_method(), str(MyClass('A').instance_method))
+print(MyClass('B').instance_method(), str(MyClass('B').instance_method))
+
+print()
+
+
+# динамическое добавление метода в класс
+def new_instance_method(self):
+    return f"вызван новый метод инстанса {self.name} класса"
+
+MyClass.new_instance_method = new_instance_method
+print(MyClass('C').new_instance_method(), str(MyClass('C').new_instance_method))
+
+
+def new_class_method(cls):
+    return f"вызван новый метод класса {cls}"
+
+MyClass.new_class_method = classmethod(new_class_method)
+print(MyClass.new_class_method(), str(MyClass.new_class_method))
+
+
+def new_static_method():
+    return "вызван новый статический метод класса"
+
+MyClass.new_static_method = staticmethod(new_static_method)
+print(MyClass.new_static_method(), str(MyClass.new_static_method))
