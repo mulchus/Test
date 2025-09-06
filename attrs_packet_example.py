24abe59a6e93e8543c0eb5425022121b0b0a5b28
mulchus <mulch@list.ru>
2025-09-06 14:53:12 +0300

    Update attrs_packet_example

--- attrs_packet_example.py
+++ attrs_packet_example.py
@@ -5,7 +5,7 @@ from attrs import asdict, define, make_class, Factory
 
 @define
 class SomeClass:
-    a_number: int = 42      # TODO: а как использовать дефолтную переменную?
+    a_number: int = 42
     list_of_numbers: list[int] = Factory(list)
 
     def hard_math(self, another_number):
