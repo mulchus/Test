e975adc972f717bac32e4e31d2a62271a22783a1
mulchus <mulch@list.ru>
2026-01-25 18:06:00 +0300

    Add tests with exceptions

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -1,5 +1,8 @@
 import httpx
 from pytest_httpx import HTTPXMock
+from contextlib import nullcontext as does_not_raise
+import pytest
+from pytest import raises
 
 
 def send_get_with_json():
@@ -29,3 +32,17 @@ def send_post_with_json():
 
 print(send_get_with_json())
 print(send_post_with_json())
+
+
+@pytest.mark.parametrize(
+    ["x", "y", "expectation"],
+    [
+        (3, 2, does_not_raise()),
+        (0, 1, does_not_raise()),
+        (1, 0, raises(ZeroDivisionError)),
+        (1, "0", raises(TypeError)),
+    ],
+)
+def test_division(x, y, expectation):
+    with expectation:
+        x / y
