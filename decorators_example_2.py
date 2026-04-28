0d18335a89ec1b2a8aa74fdabfb738d10511b3c7
mulchus <mulch@list.ru>
2026-04-28 19:48:50 +0300

    Add new example

--- decorators_example_2.py
+++ decorators_example_2.py
@@ -26,3 +26,20 @@ def hello():
     print("Hello, world!")
 
 hello()
+
+
+# декоратор с вызовом (и параметрами)
+def repeat(times):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+@repeat(3)
+def say_hello():
+    print("Hello!")
+
+say_hello()
